a5127bde5d1838d5c107cec6478b8210489f6ebd
Tom Snepvangers <tomsnep@gmail.com>
2016-05-19 14:41:15 +0200

    removed unnecessary code from motion.php

--- week-3/motion.php
+++ week-3/motion.php
@@ -2,21 +2,14 @@
     $motion = $_POST['motion'];
     
     $filename1 = 'motion.txt';
-    $filename2 = 'value.txt';
 
     $timestamp = new DateTime();
     $time = $timestamp->format("Y-m-d");
 
-    $value = "Reiger gedetecteerd";
 
     $file_data1 = $time . "\r\n";
     $file_data1 .= file_get_contents($filename1);
     file_put_contents($filename1, $file_data1);
-
-
-    $file_data2 = $value . "\r\n";
-    $file_data2 .= file_get_contents($filename2);
-    file_put_contents($filename2, $file_data2);
 
 
 ?>
